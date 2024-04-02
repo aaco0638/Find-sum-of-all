be4a0a385f6925df506f71583f75a2a9c6bef418
aaco0638 <72530702+aaco0638@users.noreply.github.com>
2024-04-01 20:45:09 -0400

    Add files via upload

---
+++ #sum of all.py
@@ -0,0 +1,6 @@
+#sum of all
+
+numbers = [1, 2, 3, 4, 5] #list of numbers
+total_sum = sum(numbers) #using the sum() function to do the calculation.
+print("the sum of all numbers is: ", total_sum) #prints the result. 
+
